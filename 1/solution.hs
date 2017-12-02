835890d1aca8f8c5ae69191dd3ab4026f7d18e70
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2017-12-02 14:36:57 +0100

    Day 2

--- 1/solution.hs
+++ 1/solution.hs
@@ -1,7 +1,8 @@
 import Data.Char
 
 adjacentPairs l = zipWith (\x y -> if x == y then (x,y) else (0,0)) l (displaced l)
-  where displaced l = drop displacement l ++ take displacement l
+  where displaced = snd $ splitList
+        splitList l = splitAt displacement l
         -- displacement = 1 -- first Star
         displacement = fromIntegral $ length l `div` 2 -- second Star
 
